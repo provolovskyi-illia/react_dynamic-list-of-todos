d6aa88ea3aba510f2ae36991cfbbf4c61fb7ad4a
Provolovskyi Illia <provolovskyi.illia@gmail.com>
2021-06-03 14:14:34 +0300

    Updates

---
+++ precache-manifest.eb29b6799770706cc26aa9687c75cc10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2304bd63633645947eb72230af50bbf4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1bca82cb87f66b42ffe0",
+    "url": "./static/css/main.e73d41c8.chunk.css"
+  },
+  {
+    "revision": "6b3fbd3467dafc90b4d2",
+    "url": "./static/js/2.5ab38983.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.5ab38983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bca82cb87f66b42ffe0",
+    "url": "./static/js/main.5986c9a8.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
